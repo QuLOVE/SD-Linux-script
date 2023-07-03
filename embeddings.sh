6b65990d99c73062b17f22ea17f19a57f761dfd7
QuLOVE <127758008+QuLOVE@users.noreply.github.com>
2023-07-04 03:40:07 +0900

    Update embeddings.sh
    added embeddings

--- embeddings.sh
+++ embeddings.sh
@@ -3,4 +3,10 @@
 # Embeddings and their IDs
 declare -A embeddings=(
     ["20068"]="badhandv4 (Mix)"
+    ["6056"]="Bad artist"
+    ["82745"]="CyberRealistic Negative"
+    ["9208"]="EasyNegative"
+    ["60938"]="negative_hand"
+    ["5637"]="Deep Negative V1.5"
+    ["5119"]="Pure Eros Face"
 )

f052e6329ac4478d969897a22241c8d2f018bc51
QuLOVE <127758008+QuLOVE@users.noreply.github.com>
2023-07-04 03:52:48 +0900

    Create upscalers.sh

---
+++ upscalers.sh
@@ -0,0 +1,4 @@
+declare -A upscalers=(
+    ["4x_Valar_v1.pth"]="https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_Valar_v1.pth"
+    ["4x_NMKD-Siax_200k.pth"]="https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NMKD-Siax_200k.pth"
+)

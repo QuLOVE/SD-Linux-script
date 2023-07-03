532aef4e8e2ce957eae122d75fb25d6dc934faba
QuLOVE <127758008+QuLOVE@users.noreply.github.com>
2023-07-04 04:22:30 +0900

    Update upscalers.sh

--- upscalers.sh
+++ upscalers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Upscalers and their links
+# Upscalers and their URLs
 declare -A upscalers=(
     ["4x_Valar_v1.pth"]="https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_Valar_v1.pth"
     ["4x_NMKD-Siax_200k.pth"]="https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NMKD-Siax_200k.pth"

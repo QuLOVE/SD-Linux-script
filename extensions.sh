abf97e0e3980f95b1e46f645c221b3c1786f51db
QuLOVE <127758008+QuLOVE@users.noreply.github.com>
2023-07-04 04:24:22 +0900

    Add files via upload

---
+++ extensions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define extensions and their URLs
+declare -A extensions=(
+    ["sd-dynamic-thresholding"]="https://github.com/mcmonkeyprojects/sd-dynamic-thresholding"
+    ["stable-diffusion-webui-composable-lora"]="https://github.com/opparco/stable-diffusion-webui-composable-lora"
+    ["depthmap2mask-update"]="https://github.com/474172261/depthmap2mask-update"
+    ["sd-face-editor"]="https://github.com/ototadana/sd-face-editor.git"
+    ["a1111-sd-webui-lycoris"]="https://github.com/KohakuBlueleaf/a1111-sd-webui-lycoris"
+    ["sd-webui-controlnet"]="https://github.com/Mikubill/sd-webui-controlnet.git"
+    ["sd-webui-depth-lib"]="https://github.com/jexom/sd-webui-depth-lib.git"
+    ["posex"]="https://github.com/hnmr293/posex"
+    ["sd-webui-roop-unlock"]="https://github.com/QuLOVE/sd-webui-roop-unlock"
+)

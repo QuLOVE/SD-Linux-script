f74532366f023cd4ab4b835efce4703be934c54a
QuLOVE <127758008+QuLOVE@users.noreply.github.com>
2023-07-03 20:17:28 +0900

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
+# Detecting language
+lang=$(locale | grep LANG | cut -d= -f2 | cut -d_ -f1)
+
+# Loading localization file
+case $lang in
+  "ja")
+    source ./locales/ja.sh
+    ;;
+  "zh")
+    source ./locales/zh.sh
+    ;;
+  *)
+    source ./locales/en.sh
+    ;;
+esac
+
+
+
 # Define colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # Define models and their IDs
-declare -A models=(
-    ["80511"]="MeinaMix v10 (Anime)"
-    ["106922"]="Hassaku v1.3 (Anime WARN)"
-    ["17233"]="AbyssOrangeMix3 (Anime)"
-    ["105924"]="CetusMix_WhaleFall2 (Anime)"
-    ["93208"]="Dark Sushi Mix 2.25D (Anime)"
-    ["100675"]="MeinaValai V4 (Anime WARN)"
-    ["57618"]="Counterfeit-V3.0 (Anime)"
-    ["90854"]="Anything V5/Ink (Anime)"
-    ["77276"]="Perfect World v4 Baked (Mix WARN)"
-    ["5036"]="AbyssOrangeMix2 ZPPP (Anime WARN)"
-    ["8474"]="AbyssOrangeMix2 ZPPP Inpaint (Anime WARN)"
-    ["93901"]="NexusMix new concep (Anime)"
-)
-
-declare -A lora_models=(
-    ["102533"]="Instant photo 拍立得/Polaroid LoRA & LoHA (3D)"
-    ["62833"]="Detail Tweaker LoRA (细节调整LoRA) (Mix)"
-)
-
-declare -A embeddings=(
-    ["20068"]="badhandv4 (Mix)"
-)
+source ./safetensor_models.sh
+source ./lora_models.sh
+source ./embeddings.sh
 
 # Function to download models
 download_models() {
@@ -37,7 +35,7 @@ download_models() {
     mkdir -p "$folder"
     cd "$folder"
     for id in "${!models[@]}"; do
-        read -p "Download ${models[$id]}? [y/N] " answer
+        read -p "$DOWNLOAD_PROMPT ${models[$id]}? [y/N] " answer
         if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
             wget "https://civitai.com/api/download/models/$id" --content-disposition
         fi
@@ -45,39 +43,34 @@ download_models() {
     cd ..
 }
 
-# Check if the script is run from the root directory
 if [[ $PWD != $HOME ]]; then
-    echo -e "${RED}Please run this script from the root directory.${NC}"
+    echo -e "${RED}$RUN_FROM_ROOT${NC}"
     exit 1
 fi
 
-# Check the Linux distribution
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
 else
-    echo -e "${RED}Not running a distribution with /etc/os-release available${NC}"
+    echo -e "${RED}$NO_OS_RELEASE${NC}"
     exit 1
 fi
 
-# Check for internet connection
 if ! ping -c 1 google.com &> /dev/null; then
-    echo -e "${RED}Internet connection not found. Please ensure you are connected to the internet before running this script.${NC}"
+    echo -e "${RED}$NO_INTERNET${NC}"
     exit 1
 fi
 
-# Check for sufficient disk space (100GB in this case)
 AVAILABLE_DISK_SPACE=$(df / | tail -1 | awk '{print $4}')
 REQUIRED_DISK_SPACE=100000000
 if (( AVAILABLE_DISK_SPACE < REQUIRED_DISK_SPACE )); then
-    echo -e "${RED}Not enough disk space. Please ensure you have at least 1GB of free disk space.${NC}"
+    echo -e "${RED}$NOT_ENOUGH_SPACE${NC}"
     exit 1
 fi
 
-# Check for and install dependencies
-for pkg in curl wget git screen ngrok; do
+for pkg in curl wget git screen ngrok jq; do
     if ! command -v $pkg &> /dev/null; then
-        echo -e "${RED}$pkg could not be found, installing...${NC}"
+        echo -e "${RED}$pkg$PKG_NOT_FOUND${NC}"
         if [[ "$OS" == "Ubuntu" ]]; then
             sudo apt-get install $pkg -y
         elif [[ "$OS" == "Arch Linux" ]]; then
@@ -89,7 +82,7 @@ for pkg in curl wget git screen ngrok; do
             exit 1
         fi
     else
-        echo -e "${GREEN}$pkg is installed${NC}"
+        echo -e "${GREEN}$pkg$PKG_INSTALLED${NC}"
     fi
 done
 
@@ -97,55 +90,61 @@ done
 if [ ! -d "stable-diffusion-webui" ]; then
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui/
 else
-    echo -e "${GREEN}stable-diffusion-webui already exists${NC}"
+    echo -e "${GREEN}$SD_EXISTS${NC}"
 fi
 
 # Check Python version and install/upgrade if necessary
 if command -v python3 &> /dev/null; then
     version=$(python3 -V 2>&1 | grep -Po '(?<=Python )(.+)')
     if [[ "$version" < "3.10.6" ]]; then
-        echo -e "${RED}Python is installed but the version is less than 3.10.6, upgrading...${NC}"
+        echo -e "${RED}$PYTHON_LESS_THAN${NC}"
         # Add commands to upgrade Python here
     else
-        echo -e "${GREEN}Python 3.10.6 is installed${NC}"
+        echo -e "${GREEN}$PYTHON_INSTALLED${NC}"
     fi
 else
-    echo -e "${RED}Python could not be found, installing Python 3.10.6...${NC}"
+    echo -e "${RED}$PYTHON_NOT_FOUND${NC}"
     # Add commands to install Python here
 fi
 
 # Check pip version and upgrade if necessary
 pip_version=$(pip3 -V | cut -d " " -f 2)
 if [[ "$pip_version" < "21.0" ]]; then
-    echo -e "${RED}pip is installed but the version is less than 21.0, upgrading...${NC}"
+    echo -e "${RED}$PIP_LESS_THAN${NC}"
     pip3 install --upgrade pip
 else
-    echo -e "${GREEN}pip is up-to-date${NC}"
+    echo -e "${GREEN}$PIP_UP_TO_DATE${NC}"
 fi
 
 # Install project dependencies
 cd stable-diffusion-webui
 pip3 install -r requirements.txt
 
-# Build xformers
-source ./venv/bin/activate
-cd repositories
-git clone https://github.com/facebookresearch/xformers.git
-cd xformers
-git submodule update --init --recursive
-pip install -r requirements.txt
-pip install -e .
+# Check if xformers is installed
+if ! python3 -c "import xformers" &> /dev/null; then
+    echo -e "${RED}$XFORMERS_NOT_FOUND${NC}"
+    # Build xformers
+    source ./venv/bin/activate
+    cd repositories
+    git clone https://github.com/facebookresearch/xformers.git
+    cd xformers
+    git submodule update --init --recursive
+    pip install -r requirements.txt
+    pip install -e .
+else
+    echo -e "${GREEN}$XFORMERS_INSTALLED${NC}"
+fi
 
 # Edit webui-user.sh
 if [ -f "../webui-user.sh" ]; then
     if grep -q "COMMANDLINE_ARGS=\"--xformers --share\"" "../webui-user.sh"; then
-        echo -e "${GREEN}webui-user.sh already contains the correct COMMANDLINE_ARGS${NC}"
+        echo -e "${GREEN}$WEBUI_USER_CORRECT${NC}"
     else
         sed -i 's/export COMMANDLINE_ARGS=""/export COMMANDLINE_ARGS="--xformers --share"/g' ../webui-user.sh
-        echo -e "${GREEN}Updated COMMANDLINE_ARGS in webui-user.sh${NC}"
+        echo -e "${GREEN}$UPDATED_WEBUI_USER${NC}"
     fi
 else
-    echo -e "${RED}webui-user.sh not found${NC}"
+    echo -e "${RED}$WEBUI_USER_NOT_FOUND${NC}"
     exit 1
 fi
 
@@ -171,9 +170,6 @@ if [ ! -d "sd-webui-controlnet" ]; then
 fi
 if [ ! -d "sd-webui-depth-lib" ]; then
     git clone https://github.com/jexom/sd-webui-depth-lib.git
-fi
-if [ ! -d "depthmap2mask-update" ]; then
-    git clone https://github.com/474172261/depthmap2mask-update
 fi
 if [ ! -d "posex" ]; then
     git clone https://github.com/hnmr293/posex
@@ -211,28 +207,28 @@ fi
 cd $HOME
 
 # Download models
-read -p "Download safetensor models? [y/N] " answer
+read -p "$DOWNLOAD_SAFETENSOR_MODELS? [y/N] " answer
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     download_models "$models" "stable-diffusion-webui/models/Stable-diffusion"
 fi
 
-read -p "Download LoRA models? [y/N] " answer
+read -p "$DOWNLOAD_LORA_MODELS? [y/N] " answer
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     download_models "$lora_models" "stable-diffusion-webui/models/LoRA"
 fi
 
-read -p "Download embeddings? [y/N] " answer
+read -p "$DOWNLOAD_EMBEDDINGS? [y/N] " answer
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     download_models "$embeddings" "stable-diffusion-webui/models/Embeddings"
 fi
 
-# Copy this script to the stable diffusion directory
-cp $0 stable-diffusion-webui/
-
 # Start webui-user.sh in a screen session
 screen -dmS SD bash -c 'cd stable-diffusion-webui; ./webui-user.sh'
 
-# Start ngrok in a screen session
+# Start ngrok in a screen session and print the public URL
 screen -dmS ngrok bash -c 'ngrok http 7860'
+sleep 5
+public_url=$(curl --silent --max-time 10 --connect-timeout 5 http://127.0.0.1:4040/api/tunnels | jq --raw-output '.tunnels[0].public_url')
+echo -e "${GREEN}$PUBLIC_URL: $public_url${NC}"
 
-echo -e "${GREEN}All tasks completed successfully.${NC}"
+echo -e "${GREEN}$ALL_TASKS_COMPLETED${NC}"

11ded99d8f162f37fc3f0c12675d458b5a8ea9bc
QuLOVE <127758008+QuLOVE@users.noreply.github.com>
2023-07-04 03:14:44 +0900

    Update lora_models.sh
    add models

--- lora_models.sh
+++ lora_models.sh
@@ -2,6 +2,12 @@
 
 # LoRA models and their IDs
 declare -A lora_models=(
-    ["102533"]="Instant photo 拍立得/Polaroid LoRA & LoHA (3D)"
-    ["62833"]="Detail Tweaker LoRA (细节调整LoRA) (Mix)"
+    ["102533"]="Instant photo 拍立得/Polaroid (3D)"
+    ["62833"]="Detail Tweaker (Mix)"
+    ["101425"]="Black See-through Bodysuit (3D+NSFW)"
+    ["109493"]="MyBreastHelper (3D+NSFW)"
+    ["90362"]="Elegant Kimono (Mix)"
+    ["7804"]="SamDoesArts (Sam Yang) (Mix)"
+    ["16576"]="epi_noiseoffset (Mix)"
+    ["90115"]="FilmGirl VELVIA 2.0"
 )
